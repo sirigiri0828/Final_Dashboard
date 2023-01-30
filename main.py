30d89e86fc288570a7acf37cf6dca4ba90c06251
sirigiri <114710483+sirigiri0828@users.noreply.github.com>
2023-01-30 04:02:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import dash
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__,external_stylesheets=external_stylesheets, use_pages=True)
+
 server = app.server
 
 app.layout = html.Div([    
